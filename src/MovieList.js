b9d6f743a98e7611a768556b84bd8c9a2b281f5a
minnecoder <sdparkin@gmail.com>
2019-03-28 22:42:56 -0500

    Added Styling

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Movie from './Movie';
 
 class MoviesList extends Component {
@@ -21,12 +22,19 @@ class MoviesList extends Component {
 
   render() {
     return (
-      <div>
+      <MovieGrid>
         {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
+      </MovieGrid>
     );
   }
 }
 
 
 export default MoviesList;
+
+const MovieGrid = styled.div`
+display: grid;
+padding: 1rem;
+grid-template-columns: repeat(6, 1fr);
+grid-row-gap: 1rem;
+`;
